1442c27d5a99cf8d19a0a36b0318cce848813714
yogesh109202 <128414694+yogesh109202@users.noreply.github.com>
2024-01-17 20:47:41 +0530

    Update ThemeProvider.js

--- app/components/ThemeProvider.js
+++ app/components/ThemeProvider.js
@@ -1,13 +1,20 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
+import { createContext, useContext } from 'react';
+const ThemeContext = createContext();
 
-const ThemeContext = React.createContext()
+export function useThemeContext(){
+
+    return useContext(ThemeContext);
+}
+
 const ThemeProvider = (props) =>{
-
+    let [global, setGlobal]=useState(true);
+    let [local, setLocal]=useState(true);
     return (
-        <React.Fragment>
-
-        </React.Fragment>
+        <ThemeContext.Provider value={{global, setGlobal, local, setLocal}}>
+        {props.children}
+        </ThemeContext.Provider>
     )
 }
 
