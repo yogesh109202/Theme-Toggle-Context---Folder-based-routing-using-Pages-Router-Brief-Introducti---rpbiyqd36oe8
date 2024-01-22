6cd4911a442146015b9089c43f4da304302a83be
yogesh109202 <128414694+yogesh109202@users.noreply.github.com>
2024-01-22 17:46:24 +0530

    Update Main.js

--- app/Main.js
+++ app/Main.js
@@ -1,16 +1,16 @@
 'use client'
-import React from 'react';
+import React, { useContext } from 'react';
 import { LocalThemedBox } from './components/LocalThemedBox';
 import { ThemeContext } from './components/ThemeProvider';
 
 const Main = () => {
-
-    return(
-        <div className={"container"} id="themed-page">
-            <p id="themed-text-container">
+    const { theme } = useContext(ThemeContext)
+    return (
+        <div className={`container bg-${theme}`} id="themed-page">
+            <p id={`themed-text-container`} className={`txt-${theme}`}>
                 lorem ipsum dolor iterit n stuff
             </p>
-            <button className="btn" id="themed-button">Themed Button</button>
+            <button className={`btn btn-${theme}`} id="themed-button">Themed Button</button>
             <LocalThemedBox />
         </div>
     )
